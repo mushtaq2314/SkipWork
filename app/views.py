980d6ead51c0ff9abf4a41e59c70b69fa86e9301
mushtaq2314 <mohammedd.mushtaq@gmail.com>
2023-08-15 23:45:47 +0530

    New

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import Order
 # Create your views here.
+from datetime import datetime,date
+import calendar
+today = date.today()
+now = datetime.now()
+month=calendar.month_abbr[now.month]
 def index(request):
     return render(request,'index.html')
 def order(request):
@@ -11,7 +16,7 @@ def order(request):
         message = request.POST['message']
         file = request.FILES['pdf']
         print(email)
-        obj = Order(customer_name=name,customer_email=email,customer_mobile=mobile,document= file,special_instructions=message)
+        obj = Order(OrderID="SW"+month+str(len(Order.objects.values())+1),customer_name=name,customer_email=email,customer_mobile=mobile,document= file,special_instructions=message,date=today)
         obj.save()
-
+    print(len(Order.objects.values()))
     return render(request,'order.html')
