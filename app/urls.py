28d9153402e25e8acc37f2e399097d87a5c48cb5
mushtaq2314 <mohammedd.mushtaq@gmail.com>
2023-08-17 22:22:40 +0530

    New with DB functionality

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',views.index,name='index'),
     path('order',views.order,name='index'),
+    path('login',views.login,name='index'),
+    path('db',views.db,name='db'),
 ]+static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Order
 # Create your views here.
 from datetime import datetime,date
@@ -6,8 +6,19 @@ import calendar
 today = date.today()
 now = datetime.now()
 month=calendar.month_abbr[now.month]
+print(month,type(month))
 def index(request):
     return render(request,'index.html')
+def login(request):
+    if(request.method=='POST'):
+        if(request.POST['username']=='SkipWorkAdmin'):
+            if(request.POST['password']=='skip!@#$'):
+                return redirect('db')
+    return render(request,'login.html')
+def db(request):
+    data  = list(Order.objects.values_list())
+    print(data)
+    return render(request,'db.html',{'data':data})
 def order(request):
     category = request.GET.get('category',None)
     if request.method =='POST':
@@ -18,7 +29,7 @@ def order(request):
         category = request.POST['category']
         file = request.FILES['pdf']
         print(email,category)
-        obj = Order(OrderID="SW"+str(len(Order.objects.values())+1),customer_name=name,customer_email=email,customer_mobile=mobile,document= file,special_instructions=message,order_date=str(today),order_category=category)
+        obj = Order(OrderID="SW-"+month+str(len(Order.objects.values())+1),customer_name=name,customer_email=email,customer_mobile=mobile,document= file,special_instructions=message,order_date=str(today),order_category=category)
         obj.save()
     print(len(Order.objects.values()))
     return render(request,'order.html',{'category':category})
