73a79ed7f94ee0bac9eb1e345feb9ce535cf09cc
mushtaq2314 <mohammedd.mushtaq@gmail.com>
2023-09-08 22:01:58 +0530

    Records added

--- app/models.py
+++ app/models.py
@@ -18,3 +18,16 @@ class Assignment(models.Model):
 # class temp(models.Model):
 #     document = models.FileField( upload_to='temp/',null=True)
     
+class Record(models.Model):
+    OrderID = models.CharField( max_length=50)
+    order_category = models.CharField(default='None',max_length=100)
+    customer_name = models.CharField( max_length=100)
+    customer_mobile = models.CharField( max_length=20)
+    customer_email = models.CharField( max_length=100)
+    customer_school = models.CharField( max_length=100)
+    special_instructions = models.CharField(default='None', max_length=1000)
+    document = models.FileField(upload_to='records/', null=True)
+    payment = models.FileField( null=True,upload_to='r_payments/')
+    order_status = models.CharField(default='None',max_length=100)
+    order_date = models.CharField(default='None',max_length=100) 
+    delivery_address = models.CharField(default='None',max_length=1000)
--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from .models import Assignment
+from .models import Assignment,Record
 from django.contrib.auth.models import User 
 from django.contrib.auth import authenticate,login,logout
 from django.core.mail import send_mail,EmailMultiAlternatives
@@ -24,18 +24,21 @@ def payment(request):
         if data['work']=='Assignment':
                 obj = Assignment(OrderID="SW-"+month+str(len(Assignment.objects.values())+1),customer_name=data['fname']+' '+data['lname'],customer_email=data['email'],customer_mobile=data['mobile'],document= request.FILES['pdf'],special_instructions=data['message'],order_date=str(today),order_category=data['category'],frontpage_instructions=data['border_instructions'],customer_school=data['school'],payment=request.FILES['payment'],delivery_address=delivery_address)
                 obj.save()
-                subject = 'An '+data['work']+' order has been recieved checkout the database for further details!'
-                name = data['fname']+' '+data['lname']
-                mobile = data['mobile']
-                recipient_list = data['email']
-                cost = request.POST['cost']
-                message =f'\n\nName: {name}\nEmail: {recipient_list}\nMobile: {mobile}\nSchool:{obj.customer_school}\nCost:{cost}'
-                email_from = settings.EMAIL_HOST_USER
-                email = EmailMultiAlternatives(subject,message,email_from,[email_from])
-                # email.attach_file(f'./media/{obj.payment}')
-                email.attach_file(f'/home/skipwork/SkipWork/media/{obj.payment}')
-                email.send()
-                request.session.pop('data',None)
+        if data['work']=='Record':
+            obj = Record(OrderID="SW-"+month+str(len(Assignment.objects.values())+1),customer_name=data['fname']+' '+data['lname'],customer_email=data['email'],customer_mobile=data['mobile'],document= request.FILES['pdf'],special_instructions=data['message'],order_date=str(today),order_category=data['category'],customer_school=data['school'],payment=request.FILES['payment'],delivery_address=delivery_address)
+            obj.save()
+        subject = 'An '+data['work']+' order has been recieved checkout the database for further details!'
+        name = data['fname']+' '+data['lname']
+        mobile = data['mobile']
+        recipient_list = data['email']
+        cost = request.POST['cost']
+        message =f'\n\nName: {name}\nEmail: {recipient_list}\nMobile: {mobile}\nSchool:{obj.customer_school}\nCost:{cost}'
+        email_from = settings.EMAIL_HOST_USER
+        email = EmailMultiAlternatives(subject,message,email_from,[email_from])
+        # email.attach_file(f'./media/{obj.payment}')
+        email.attach_file(f'/home/skipwork/SkipWork/media/{obj.payment}')
+        email.send()
+        request.session.pop('data',None)
                 # print(len(Assignment.objects.values()))
             
     return render(request,'done.html')
